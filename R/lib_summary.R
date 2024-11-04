16e2a49c0bd75936514f17756808f5a832409fb4
jamiectam <jamiectam.phd@gmail.com>
2024-11-04 16:04:09 -0400

    add roxygen

---
+++ R/lib_summary.R
@@ -0,0 +1,17 @@
+#' Provide Number of R Packages by Library
+#'
+#' Provide the number of R package by library in
+#'  a data.frame
+#'
+#' @return a data.frame of R packages by library
+#' @export
+#'
+#' @examples
+#' lib_summary()
+lib_summary <- function() {
+  pkgs <- utils::installed.packages()
+  pkg_tbl <- table(pkgs[, "LibPath"])
+  pkg_df <- as.data.frame(pkg_tbl, stringsAsFactors = FALSE)
+  names(pkg_df) <- c("Library", "n_packages")
+  pkg_df
+}
