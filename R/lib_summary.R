7a63c0ef1eed8bd140f927aa940dfccb4752d5b3
jamiectam <jamiectam.phd@gmail.com>
2024-11-05 14:49:38 -0400

    import dependencies using use_import_from()

--- R/lib_summary.R
+++ R/lib_summary.R
@@ -1,17 +1,28 @@
-#' Provide Number of R Packages by Library
+#' R Library Summary
 #'
-#' Provide the number of R package by library in
-#'  a data.frame
+#' Provides a brief summary of the packagelibraries on your machine
+#'
+#' @param sizes Should sizes of libraries be calculated. Default 'FALSE'.
 #'
 #' @return a data.frame of R packages by library
 #' @export
 #'
 #' @examples
 #' lib_summary()
-lib_summary <- function() {
+lib_summary <- function(sizes = FALSE) {
+  if (!is.logical(sizes)) {
+    stop("'sizes' must be logical (TRUE or FALSE)")
+  }
   pkgs <- utils::installed.packages()
   pkg_tbl <- table(pkgs[, "LibPath"])
   pkg_df <- as.data.frame(pkg_tbl, stringsAsFactors = FALSE)
   names(pkg_df) <- c("Library", "n_packages")
+
+  if (isTRUE(sizes)) {
+    pkg_df$lib_size <- map_dbl(
+      pkg_df$Library,
+      \(x) sum(fs::file_size(fs::dir_ls(x, recurse = TRUE)))
+    )
+  }
   pkg_df
 }
